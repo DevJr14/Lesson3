3e3f79ab4d7822b0e4fc67b2a40a1ee858294239
Junior Matlou <Junianohmm6@gmail.com>
2022-08-03 21:35:58 +0200

    Add project files.

---
+++ Lesson3/Controllers/StudentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet("get-age")]
+        public ActionResult GetAge()
+        {
+            return Ok(44);
+        }
+    }
+}
